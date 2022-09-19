a659214c8f7ad0fc0c98274a8980f6c6611d34e9
gkze <george.kontridze@gmail.com>
2022-09-19 15:17:31 -0700

    Cleanup, JSON output
    - Clean up trending repository accumulation logic to be more terse
    - Use native github.com/google/go-github repository struct
    - Add JSON output formatting

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/cli/go-gh v0.1.0
+	github.com/google/go-github/v47 v47.1.0
 	github.com/spf13/cobra v1.5.0
 )
 
@@ -12,13 +13,15 @@ require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/cli/browser v1.1.0 // indirect
 	github.com/cli/safeexec v1.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/rivo/uniseg v0.4.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	golang.org/x/crypto v0.0.0-20220919173607-35f4265a4bc0 // indirect
+	golang.org/x/net v0.0.0-20220919171627-f8f703f97925 // indirect
 	golang.org/x/sys v0.0.0-20220913175220-63ea55921009 // indirect
 	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -12,31 +17,30 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/cli/go-gh/pkg/browser"
+	"github.com/cli/go-gh/pkg/jsonpretty"
 	"github.com/cli/go-gh/pkg/tableprinter"
 	"github.com/cli/go-gh/pkg/term"
+	"github.com/google/go-github/v47/github"
 	"github.com/spf13/cobra"
 )
 
+const trendingBaseURL string = "https://github.com/trending"
+
+var cutset *regexp.Regexp = regexp.MustCompile("(,|\n| |/)")
+
+type outputFormat string
+
 const (
-	trendingBaseURL string = "https://github.com/trending"
-	cutset          string = ",\n /"
+	JSON  outputFormat = "json"
+	table outputFormat = "table"
 )
 
-type Trending struct {
-	Owner       *string
-	Name        *string
-	Language    *string
-	Stars       *int64
-	Description *string
-}
+var (
+	openInBrowser *bool
+	outFormat     *string
+)
 
-func (t *Trending) URL() string {
-	return fmt.Sprintf("https://github.com/%s/%s", *t.Owner, *t.Name)
-}
-
-var openInBrowser *bool
-
-func GetTrending(language string) ([]Trending, error) {
+func GetTrending(language string) ([]github.Repository, error) {
 	res, err := http.Get(fmt.Sprintf("%s/%s", trendingBaseURL, language))
 	if err != nil {
 		return nil, err
@@ -51,7 +55,7 @@ func GetTrending(language string) ([]Trending, error) {
 		return nil, err
 	}
 
-	trendings := []Trending{}
+	trendings := []github.Repository{}
 	doc.Find("article.Box-row").Each(func(i int, s *goquery.Selection) {
 		href, exists := s.Find("h1.h3.lh-condensed > a").Attr("href")
 		if !exists {
@@ -65,47 +69,48 @@ func GetTrending(language string) ([]Trending, error) {
 			return
 		}
 
-		stargazersStr := strings.Trim(
-			s.Find(fmt.Sprintf("a[href=\"%s/stargazers\"]", href)).Text(),
-			cutset,
-		)
-		var stargazers int64 = 0
-		if stargazersStr != "" {
-			stargazers, err = strconv.ParseInt(
-				strings.ReplaceAll(stargazersStr, ",", ""), 10, 64,
-			)
-			if err != nil {
-				log.Printf("Could not parse %v as int64", stargazersStr)
-			}
-		}
-
-		lang := strings.Trim(
-			s.Find("span[itemprop=\"programmingLanguage\"]").Text(), cutset,
-		)
-
-		desc := strings.Trim(
-			s.Find("p.col-9").Text(), cutset,
-		)
-
-		trending := Trending{
-			Owner: &ownerRepo[1],
+		trendings = append(trendings, github.Repository{
+			Owner: &github.User{Login: &ownerRepo[1]},
 			Name:  &ownerRepo[2],
-			Stars: &stargazers,
-		}
-
-		if lang != "" {
-			trending.Language = &lang
-		}
-
-		if desc != "" {
-			trending.Description = &desc
-		}
-
-		trendings = append(trendings, trending)
+			StargazersCount: func() *int {
+				stargazersInt64, err := strconv.ParseInt(
+					cutset.ReplaceAllString(
+						s.Find(
+							fmt.Sprintf("a[href=\"%s/stargazers\"]", href),
+						).Text(),
+						"",
+					),
+					10, 64,
+				)
+				if err != nil {
+					panic(err)
+				}
+				stargazersInt := int(stargazersInt64)
+				return &stargazersInt
+			}(),
+			Language: func() *string {
+				var lang string
+				if lang = cutset.ReplaceAllString(
+					s.Find("span[itemprop=\"programmingLanguage\"]").Text(), "",
+				); lang != "" {
+					return &lang
+				}
+				return nil
+			}(),
+			Description: func() *string {
+				var desc string
+				if desc = strings.Trim(
+					s.Find("p.col-9").Text(), "\n ",
+				); desc != "" {
+					return &desc
+				}
+				return nil
+			}(),
+		})
 	})
 
 	sort.Slice(trendings, func(i, j int) bool {
-		return *(trendings[i].Stars) > *(trendings[j].Stars)
+		return *(trendings[i].StargazersCount) > *(trendings[j].StargazersCount)
 	})
 
 	return trendings, nil
@@ -116,6 +121,10 @@ var trendingCmd cobra.Command = cobra.Command{
 	Short: "Show trending repositories",
 	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if *openInBrowser && outputFormat(*outFormat) == JSON {
+			log.Fatal("Cannot select output format when opening in browser")
+		}
+
 		trendings, err := GetTrending(func() string {
 			if len(args) == 1 {
 				return args[0]
@@ -136,47 +145,66 @@ var trendingCmd cobra.Command = cobra.Command{
 					if err := b.Browse(u); err != nil {
 						panic(err)
 					}
-				}(trending.URL())
+				}(trending.GetHTMLURL())
 			}
 			wg.Wait()
 
 			return nil
 		}
 
-		width, _, err := term.FromEnv().Size()
-		if err != nil {
-			return err
-		}
+		switch outputFormat(*outFormat) {
+		case JSON:
+			var b bytes.Buffer
+			trendingJSONbuf := bufio.NewReadWriter(
+				bufio.NewReader(&b), bufio.NewWriter(&b),
+			)
+			if err := json.NewEncoder(trendingJSONbuf).Encode(trendings); err != nil {
+				return err
+			}
 
-		tp := tableprinter.New(os.Stdout, term.IsTerminal(os.Stdout), width)
-		for _, header := range []string{
-			"OWNER", "NAME", "LANG", "URL", "STARS", "DESC",
-		} {
-			tp.AddField(header)
-		}
-		tp.EndRow()
+			if err := jsonpretty.Format(
+				io.Writer(os.Stdout), io.Reader(trendingJSONbuf), " ", true,
+			); err != nil {
+				return err
+			}
+		case table:
+			width, _, err := term.FromEnv().Size()
+			if err != nil {
+				return err
+			}
 
-		for _, trending := range trendings {
-			tp.AddField(*trending.Owner)
-			tp.AddField(*trending.Name)
-			tp.AddField(func() string {
-				if trending.Language != nil {
-					return *trending.Language
-				}
-				return ""
-			}())
-			tp.AddField(trending.URL())
-			tp.AddField(fmt.Sprint(*trending.Stars))
-			tp.AddField(func() string {
-				if trending.Description != nil {
-					return *trending.Description
-				}
-				return ""
-			}())
+			tp := tableprinter.New(os.Stdout, term.IsTerminal(os.Stdout), width)
+			for _, header := range []string{
+				"OWNER", "NAME", "LANG", "URL", "STARS", "DESC",
+			} {
+				tp.AddField(header)
+			}
 			tp.EndRow()
+
+			for _, trending := range trendings {
+				tp.AddField(*trending.Owner.Login)
+				tp.AddField(*trending.Name)
+				tp.AddField(func() string {
+					if trending.Language != nil {
+						return *trending.Language
+					}
+					return ""
+				}())
+				tp.AddField(trending.GetHTMLURL())
+				tp.AddField(fmt.Sprint(*trending.StargazersCount))
+				tp.AddField(func() string {
+					if trending.Description != nil {
+						return *trending.Description
+					}
+					return ""
+				}())
+				tp.EndRow()
+			}
+
+			return tp.Render()
 		}
 
-		return tp.Render()
+		return nil
 	},
 }
 
@@ -184,6 +212,9 @@ func init() {
 	log.SetOutput(os.Stderr)
 	openInBrowser = trendingCmd.PersistentFlags().BoolP(
 		"web", "w", false, "Open in web browser",
+	)
+	outFormat = trendingCmd.PersistentFlags().StringP(
+		"output", "o", string(table), "Output format (valid: json, table)",
 	)
 }
 
